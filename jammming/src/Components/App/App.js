83c3656398a5345802269cbad204b48baccf834b
Bea <btmaj8@gmail.com>
2018-03-19 16:20:02 +0100

    Setting up the project

---
+++ jammming/src/Components/App/App.js
@@ -0,0 +1,87 @@
+import React from 'react';
+import {SearchBar} from '../SearchBar/SearchBar';
+import {SearchResults} from '../SearchResults/SearchResults';
+import {PlayList} from '../PlayList/PlayList';
+import {Spotify} from '../../util/Spotify';
+import './App.css';
+
+export class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchResults: [],
+      playlistName: '',
+      playlistTracks: []
+    };
+
+    this.addTrack = this.addTrack.bind(this);
+    this.removeTrack = this.removeTrack.bind(this);
+    this.updatePlayListName = this.updatePlayListName.bind(this);
+    this.savePlayList = this.savePlayList.bind(this);
+    this.search = this.search.bind(this);
+  }
+
+  addTrack(track, isRemoval) {
+    if (this.state.playlistTracks.filter(item => track.id !== item.id)) {
+      let newPlayList = this.state.playlistTracks;
+      newPlayList.push(track);
+      this.setState({
+        playlistTracks: newPlayList
+      });
+  }}
+
+  removeTrack(track) {
+    let newPlayList = this.state.playlistTracks.filter(item => track.id !== item.id);
+    this.setState({
+      playlistTracks: newPlayList
+    });
+  }
+
+  updatePlayListName(name) {
+    this.setState({
+      playlistName: name
+    });
+  }
+
+  savePlayList() {
+    let trackURIs = this.state.playlistTracks.map(track => track.uri);
+    Spotify.savePlayList(this.state.playlistName, trackURIs).then(() => {
+     this.setState({
+       playlistName: 'New Playlist',
+       searchResults: []
+     });
+   });
+  }
+
+  search(searchTerm) {
+    Spotify.search(searchTerm).then(searchResults => {
+      this.setState({searchResults: searchResults});
+    });
+  }
+
+  verify(searchTerm) {
+    if (Spotify.accessToken === null || Spotify.accessToken === '' ) {
+      Spotify.getAccessToken();
+    } else {
+      console.log('Already Verified');
+      Spotify.search(searchTerm);
+    }
+  }
+
+  render() {
+    window.onload = this.verify();
+    return (
+      <div>
+        <h1>Ja<span className="highlight">mmm</span>ing</h1>
+        <div className="App">
+          <SearchBar onSearch={this.search} />
+          <div className="App-playlist">
+            <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack} />
+            <PlayList playlistName={this.state.playlistName} playlistTracks={this.state.playlistTracks} onRemove={this.removeTrack} onNameChange={this.updatePlayListName} onSave={this.savePlayList} />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
